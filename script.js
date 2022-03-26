76d4a06a6a551c754f00f1cd652f6f7875ae5ea7
disha anand <dishaanand2299@gmail.com>
2022-03-26 23:59:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const form =document.getElementById("form")
+const input=document.getElementById("input")
+const todos=document.getElementById("todos")
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    addtodo();
+
+
+})
+
+    function addtodo(){
+    const todoText=input.value
+    if(todoText){
+        const todoEl=document.createElement("li");
+        todoEl.innerText=todoText;
+
+        todoEl.addEventListener('click',()=>{
+            todoEl.classList.toggle('completed');
+        });
+
+        todoEl.addEventListener("contextmenu",
+        (e)=>{
+
+            todoEl.remove();
+        });
+
+
+
+        todos.appendChild(todoEl);
+
+        input.value='';
+
+    }
+}
+function updateLS(){
+    const todosEl=documnet.querySelectorAll("li")
+
+}
+
